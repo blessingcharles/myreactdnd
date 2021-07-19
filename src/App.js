3067b5da54a780681adf574d3c46ff3ffe9f72b2
blessingcharles <charlesblessing001@gmail.com>
2021-07-19 13:25:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [isPhoneNumber , setPhoneNumber ] = useState(false);
+  const [isEmail , setEmail] = useState(true);
+
+  const emailHandler = ()=>{
+    setEmail(prev =>{
+      return !prev
+    })
+  }
+
+  const phoneHandler = ()=>{
+      setPhoneNumber(prev =>{
+        return !prev
+      })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+     <i>Email :</i><input type="checkbox" checked={isEmail} onClick={emailHandler}/>
+     <i>PhoneNumber : </i><input type="checkbox" checked={isPhoneNumber} onClick={phoneHandler}/> 
+     <div className="login-page" >
+            <div className="form">
+                 <form className="login-form characters">
+                   
+                 <div className="container">
+                    <p className="draggable" draggable="true">
+                     { 
+                                    (isEmail)?<input type="text" placeholder="Email"/>:null
+                                  }
+
+                                  <input type="password" placeholder="Password"/>
+                    </p>
+                    <p className="draggable" draggable="true">
+                                   { (isPhoneNumber)?
+                                  <input type="text" placeholder="Phone Number" />:null
+                                  }
+                    </p>
+                  </div>
+
+                       <button>Login</button>
+                        <p className="message">New User ? <a href="#">SignUp</a></p>
+                 </form>
+      </div>
+      </div>
+  </>
   );
+
 }
 
 export default App;
